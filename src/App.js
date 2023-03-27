43f680d95aa468d3b68a1433b0cb864c371a3530
Ismail zahar <zaharsmail@gmail.com>
2023-03-27 11:23:18 +0100

    review project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './componnents/MyForm';
+import Mynavbar from './componnents/Mynavbar';
+function App() {
+  return (
+    <>
+       <Mynavbar />
+      <MyForm />
+   
+    </>
+  );
+}
+
+export default App;
